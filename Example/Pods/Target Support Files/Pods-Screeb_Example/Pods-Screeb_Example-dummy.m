5e47efafa61a5447d5f4c0ea8ace0f6d476bef47
Clement <clement@screeb.app>
2021-11-26 14:57:49 +0100

    clean repo

--- Example/Pods/Target Support Files/Pods-Screeb_Example/Pods-Screeb_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Screeb_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Screeb_Example
-@end
--- Example/Pods/Target Support Files/Screeb/Screeb-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Screeb : NSObject
-@end
-@implementation PodsDummy_Screeb
-@end
