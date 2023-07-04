2d7db446b0210d015fcd8e657f571e266bd20e5f
Alex <alexisr245@gmail.com>
2023-07-04 10:48:46 +0200

    bump 2.0.0-rc.1

--- Package.swift
+++ Package.swift
@@ -12,31 +12,19 @@ let package = Package(
             name: "Screeb",
             targets: ["ScreebTarget"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(name: "Alamofire",url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.0"),
-		.package(name: "Starscream",url: "https://github.com/daltoniam/Starscream.git", from: "4.0.0"),
-		.package(name: "Kingfisher",url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
-		.package(name: "KeyboardGuide",url: "https://github.com/niw/KeyboardGuide.git", from: "0.2.1"),
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "ScreebTarget",
             dependencies: [.target(name: "BinaryFrameworkWrapper")],
             path: "Sources/Screeb"),
         .target(name: "BinaryFrameworkWrapper",
-               dependencies: [
-                .target(name: "Screeb"),
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "Starscream", package: "Starscream"),
-                .product(name: "Kingfisher", package: "Kingfisher"),
-                .product(name: "KeyboardGuide", package: "KeyboardGuide")
-               ],
+               dependencies: [],
                path: "Sources/BinaryFramework"),
         .binaryTarget(
             name: "Screeb",
-            url: "https://github.com/ScreebApp/sdk-ios-public/releases/download/1.13.2/Screeb.zip",
-            checksum: "bc2c69eaf305201ecc79476c48a90f6d38beffb980f07e940f7a3edba203f88c"
+            url: "https://github.com/ScreebApp/sdk-ios-public/releases/download/2.0.0-rc.1/Screeb.zip",
+            checksum: "1f2dcd1c1bb48596b156cd0731ccbbdf908fdc15d58c85a7ee9b71e5b31ee32c"
         )
     ]
 )
