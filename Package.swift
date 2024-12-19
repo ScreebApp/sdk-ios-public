05ef09cad14c3862f6e1db97c5ae240ebc10c055
Alexis Rouillard <alexisr245@gmail.com>
2024-12-19 10:00:53 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
                path: "Sources/BinaryFramework"),
         .binaryTarget(
             name: "Screeb",
-            url: "https://github.com/ScreebApp/sdk-ios-public/releases/download/2.1.8/Screeb.zip",
-            checksum: "edfab2ce652f18d3f44278dc69950dd17fa6bcfd600585aef15dd87e8999bde0"
+            url: "https://github.com/ScreebApp/sdk-ios-public/releases/download/2.1.9/Screeb.zip",
+            checksum: "3645acdb945e422929a6113ef7d1eeca288640c642b5caa71198163d873b119f"
         )
     ]
 )
